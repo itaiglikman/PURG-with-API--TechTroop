a3b414d0c34c3153563aa80a781d77bb44ec0afc
itaiglikman <itaig1998@gmail.com>
2025-07-30 13:54:44 +0300

    feat: response status class

---
+++ utils/resonseStatus.js
@@ -0,0 +1,13 @@
+class ResponseStatus {
+    static OK = 200;
+    static CREATED = 201;
+    static ACCEPTED = 202;
+    static NO_CONTENT = 204;
+    static BAD_REQUEST = 400;
+    static UNAUTHORIZED = 401;
+    static FORBIDDEN = 403;
+    static NOT_FOUND = 404;
+    static INTERNAL_SERVER_ERROR = 500;
+}
+
+export default ResponseStatus;
