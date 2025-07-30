0c3b7574c97fbbc1a838e4806dd67443ed06b63a
itaiglikman <itaig1998@gmail.com>
2025-07-30 16:56:01 +0300

    feat: create renderer

--- view/Renderer.js
+++ view/Renderer.js
@@ -1,1 +1,37 @@
-// handles changes in the dom
+function Renderer(state) {
+
+    const mainUserName = $('#main-user-name');
+    const mainUserLocation = $('#main-user-location');
+    const mainUserImage = $('#main-user-image');
+    const friendsUl = $('#friends-list');
+    $(friendsUl).empty(); //empty ul before every loading
+    const quote = $('#quote-text');
+    const pokemonImage = $('#pokemon-image');
+    const pokemonName = $('#pokemon-name');
+    const aboutMe = $('#aboutme-text');
+
+    // main user header:
+    $(mainUserName).text(state.mainUser.getFullName());
+    $(mainUserLocation).text(state.mainUser.getFullAddress());
+    $(mainUserImage).attr('src', state.mainUser?.imageURL);
+    
+    // friends side bar:
+    state.friends.forEach(f => {
+        const li = $('<li>').text(f.getFullName());
+        $(friendsUl).append(li);
+    });
+    
+    // quote:
+    $(quote).text(state.quote);
+    
+    // pokemon:
+    $(pokemonName).text(state.pokemon.name);
+    $(pokemonImage).attr('src', state.pokemon?.imageURL);
+    
+    // aboutMe:
+    $(aboutMe).text(state.aboutMe);
+
+
+}
+
+export default Renderer;
