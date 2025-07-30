f5a1a056bc2ebf0c4806dbd7d7b7ad8dc0b2381d
itaiglikman <itaig1998@gmail.com>
2025-07-30 14:49:00 +0300

    feat: user model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,17 @@
-// single user model
+class User {
+
+    constructor(apiUser) {
+        this.firstName = apiUser.name.first;
+        this.lastName = apiUser.name.last;
+        this.city = apiUser.location.city;
+        this.country = apiUser.location.country;
+        this.imageURL = apiUser.picture.medium;
+    }
+
+    getFullname() {
+        return this.firstName + this.lastName;
+    }
+
+}
+
+export default User;
