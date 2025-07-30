082e5993d089fa1ee37a0c04752a96255a4c4dac
itaiglikman <itaig1998@gmail.com>
2025-07-30 14:34:35 +0300

    feat: get meat api

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 // control all parts of the app
+import APIManager from "./models/APIManager.js";
+
+const api = new  APIManager();
+
+
+
+
+async function render(){
+    try {
+        
+        const users = await api.getUsers();
+        const quote = await api.getQuote();
+        const pokemon = await api.getRandomPokemon();
+        const meat = await api.getMeat();
+        
+        console.log(users);
+        console.log(quote);
+        console.log(pokemon);
+        console.log(meat);
+    } catch (error) {
+        console.error(error.message);
+        throw new Error(error);
+    }
+}
+
+render();
--- models/APIManager.js
+++ models/APIManager.js
@@ -1,14 +1,16 @@
-import ErrorMsg from "../utils/errors.js";
-import ResponseStatus from "../utils/resonseStatus.js";
-
 class APIManager {
     constructor() {
-        // this.usersAPI = 'https://randomuser.me/api/';
         this.usersAPI = 'https://randomuser.me/api/?results=7/';
         this.quoteAPI = 'https://api.kanye.rest/';
         this.randPokemonApi = 'https://pokeapi.co/api/v2/pokemon/';
+        this.meatAPI = 'https://baconipsum.com/api/?type=all-meat/';
     }
 
+    //handle errors
+
+    /**
+     * @returns array with 7 random users objets
+     */
     async getUsers() {
         try {
             const response = await axios.get(this.usersAPI);
@@ -20,6 +22,9 @@ class APIManager {
         }
     }
 
+    /**
+     * @returns string of a random quote
+     */
     async getQuote() {
         try {
             const response = await axios.get(this.quoteAPI);
@@ -31,20 +36,40 @@ class APIManager {
         }
     }
 
+    /**
+     * get full random pokemon object 
+     * @returns object with name and image url
+     */
     async getRandomPokemon() {
         try {
             const randNum = Math.floor(Math.random() * 1026);
             const response = await axios.get(this.randPokemonApi + randNum.toString());
             const pokemonName = response.data.forms[0].name;
             const pokemonImageUrl = response.data.sprites.front_default;
-            const pokemon = {name:pokemonName,imageURL:pokemonImageUrl};
+            const pokemon = { name: pokemonName, imageURL: pokemonImageUrl };
             return pokemon;
         } catch (error) {
             console.error(error.message);
             throw new Error(error);
         }
-
     }
-}
 
+    /**
+     * get array of sentences
+     * @returns string of a paragraph
+     */
+    async getMeat() {
+        try {
+            const response = await axios.get(this.meatAPI);
+            const meat = response.data;
+            let meatPara = '';
+            meat.forEach(item => meatPara += item);
+            return meatPara;
+        } catch (error) {
+            console.error(error.message);
+            throw new Error(error);
+        }
+    }
+
+}
 export default APIManager;
