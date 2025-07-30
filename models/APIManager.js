9d010a46d902e24ecd82ad1f40fdbedc15ea3361
itaiglikman <itaig1998@gmail.com>
2025-07-30 13:29:43 +0300

    get users from api

--- models/APIManager.js
+++ models/APIManager.js
@@ -1,1 +1,19 @@
-// handles all api request and received data
+class APIManager {
+    constructor() {
+        this.usersAPI = 'https://randomuser.me/api/?results=7';
+        this.quoteAPI = 'https://api.kanye.rest';
+    }
+
+    async getUsers() {
+        try {
+            const response = await axios.get(this.usersAPI);
+            const users = response.data.results
+            return users;
+        } catch (error) {
+            console.error(error.message);
+            throw new Error(error);
+        }
+    }
+}
+
+export default APIManager;
