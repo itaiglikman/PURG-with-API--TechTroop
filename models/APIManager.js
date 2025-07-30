fbe3a7642a0ffdd4b89b16107d1544a5c94e0a34
itaiglikman <itaig1998@gmail.com>
2025-07-30 13:56:17 +0300

    feat: quote api

--- models/APIManager.js
+++ models/APIManager.js
@@ -1,5 +1,9 @@
+import ErrorMsg from "../utils/errors.js";
+import ResponseStatus from "../utils/resonseStatus.js";
+
 class APIManager {
     constructor() {
+        // this.usersAPI = 'https://randomuser.me/api/';
         this.usersAPI = 'https://randomuser.me/api/?results=7';
         this.quoteAPI = 'https://api.kanye.rest';
     }
@@ -7,8 +11,27 @@ class APIManager {
     async getUsers() {
         try {
             const response = await axios.get(this.usersAPI);
-            const users = response.data.results
+            if (response.status !== ResponseStatus.OK)
+                throw ErrorMsg.invalidResponse(response.status);
+            const users = response.data.results;
+            if (!users || users.length === 0)
+                throw ErrorMsg.noUsersFound();
             return users;
+        } catch (error) {
+            console.error(error.message);
+            throw new Error(error);
+        }
+    }
+
+    async getQuote() {
+        try {
+            const response = await axios.get(this.quoteAPI);
+            if (response.status !== ResponseStatus.OK)
+                throw ErrorMsg.invalidResponse(response.status);
+            const quote = response.data.quote;
+            if (!quote)
+                throw ErrorMsg.noQuote();
+            return quote;
         } catch (error) {
             console.error(error.message);
             throw new Error(error);
